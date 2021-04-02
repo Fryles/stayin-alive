0f2514202f09521308663867302c3ca79e6c79d0
fryles <mylesmarr@gmail.com>
2021-04-02 10:40:09 -0700

    Added some theming instead of placeholders & almost complete posting

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
-
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs')
 
 app.use(express.static(__dirname + '/'));
 
+app.get('/explore', (req, res) => {
+	fs.readFile('./timetable.json' , 'utf8', (err, data) => {
+		if (err) throw err;
+		data = JSON.parse(data);
+		index = req.query.index
+		res.status(200).end(data[index]);
+	})
+	
+});
+
+
+
 app.listen(80, () => {
-	console.log('Server is listening on port 80')
+	console.log('Server is listening on port 80');
 });
--- script.js
+++ script.js
@@ -1,13 +1,71 @@
 document.addEventListener("DOMContentLoaded", function () {
   loadExplore(15);
 });
+
+function postDia() {
+  //shennanigans to give input an onchnage
+  var wrapper = document.createElement("div");
+  wrapper.innerHTML =
+    '<input type="text" id="Title" placeholder="Title"><input type="text" id="Desc" placeholder="Description"><input type="file" onchange="changeDis(this)">';
+  swal({
+    title: "Upload your photo",
+    icon: "placeholder.png",
+    content: wrapper,
+    buttons: ["Cancel", "Do it!"],
+  }).then((result) => {
+    if (result) {
+      post();
+    }
+  });
+  //make custom img easier to find and change as user uploads img
+  $(".swal-icon--custom").children()[0].id = "photoDis";
+}
+function post() {
+  var img = $("#photoDis").attr("src");
+  var title = $("#Title").val();
+  var desc = $("#Desc").val();
+  toDataURL(img, function(dataUrl) {
+    img = dataUrl;
+    $.ajax({
+      url: "./post",
+      type: "POST",
+      data: {
+        'img':img,
+        'title':title,
+        'desc':desc,
+    },
+      dataType: "json",
+      error: function (err) {
+        //swal
+        console.log("Error:" + err);
+      },
+      success: function (msg) {
+        //swal
+        console.log(msg);
+      },
+    });
+  })
+  
+}
+function changeDis(input) {
+  console.log(input);
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $("#photoDis").attr("src", e.target.result).width(200).height(200);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
 function loadExplore(reqs = 10) {
+  //CLEAR items
   //TODO move list of posts to server to stop race condition
   for (i = 0; i < reqs; i++) {
     $.ajax({
       url: "./explore",
       type: "GET",
-      data: i.toString(),
+      data: { index: i },
       dataType: "html",
       error: function (err) {
         console.log("Error:" + err);
@@ -17,4 +75,20 @@ function loadExplore(reqs = 10) {
       },
     });
   }
+}
+
+//helper func for sending imgs over ajax
+//https://stackoverflow.com/questions/6150289/how-can-i-convert-an-image-into-base64-string-using-javascript
+function toDataURL(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    var reader = new FileReader();
+    reader.onloadend = function() {
+      callback(reader.result);
+    }
+    reader.readAsDataURL(xhr.response);
+  };
+  xhr.open('GET', url);
+  xhr.responseType = 'blob';
+  xhr.send();
 }
